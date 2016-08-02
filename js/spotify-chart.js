c9d7354002793d5b77c7b93622825bbf741c8a73
Savannah Scott <sascott624@gmail.com>
2016-08-02 20:59:02 +0000

    Done.

--- js/spotify-chart.js
+++ js/spotify-chart.js
@@ -1,9 +1,9 @@
 var url = "https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/top-tracks?country=SE";
 
 var dataSetProperties = {
-  fillColor: 'rgba(220,220,220,0.5)', 
-  strokeColor: 'rgba(220,220,220,0.8)', 
-  highlightFill: 'rgba(220,220,220,0.75)', 
+  fillColor: 'rgba(220,220,220,0.5)',
+  strokeColor: 'rgba(220,220,220,0.8)',
+  highlightFill: 'rgba(220,220,220,0.75)',
   highlightStroke: 'rgba(220,220,220,1)'
 };
 
@@ -15,41 +15,76 @@ $(function() {
 // then call function within doc ready to get them to work
 // and display the chart correctly in index.html
 
+
 function extractTop10Tracks(tracks) {
-  // your code here
+  var trackArray = $.map(tracks, function(track, index){
+    return track;
+  });
+  return trackArray;
 }
 
+//DATA for chart
 function extractPopularity(tracks) {
-  // your code here
+  var trackPopularity = $.map(tracks, function(track, index){
+    return track.popularity;
+  });
+  return trackPopularity;
 }
 
+//LABELS for chart
 function extractNames(tracks) {
-  // your code here
+  var trackName = $.map(tracks, function(track, index){
+    return track.name;
+  });
+  return trackName;
 }
 
+//labels = extractNames(tracks) && inputData = extractPopularity(tracks)
 function chartData(labels, inputData) {
-  // your code here
-
-  // use the dataSetProperties variable defined above if it helps
+  var chart = {
+    "labels": labels,
+    "datasets": [
+      {"fillColor": dataSetProperties.fillColor,
+      "strokeColor": dataSetProperties.strokeColor,
+      "highlightFill": dataSetProperties.highlightFill,
+      "highlightStroke": dataSetProperties.highlightStroke,
+      "data": inputData}
+    ]
+  };
+  return chart;
 }
 
 function getSpotifyTracks(callback){
-  // your ajax call here, on success it should call on the 
-  // parameter it's passed (it's a function), and pass it's 
+  // your ajax call here, on success it should call on the
+  // parameter it's passed (it's a function), and pass its
   // parameter the data it received
 
-  // use the url variable defined above if it helps
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: "json",
+    success: function(response){
+      callback(response);
+    }
+  })
 }
 
+//parsedJSON starts as the results from the api GET request above
 function success(parsedJSON) {
   // this function will make a new bar chart, refer to this url:
   // http://www.chartjs.org/docs/#bar-chart
   // you will need to call on:
   //  1. extractTop20Tracks - pass it tracks
+  var tracks = extractTop10Tracks(parsedJSON.tracks);
   //  2. extractNames -  pass it the result of #1
+  var names = extractNames(tracks);
   //  3. extractPopularity - pass it the result of #1
+  var popularity = extractPopularity(tracks);
   //  4. chartData - pass it results of #2 and #3
+  var myChartData = chartData(names, popularity);
   //  5. make a variable `ctx` and select the canvas with the id of spotify-chart
   //     * also make sure to specify 2d context
+  var ctx = document.getElementById("spotify-chart").getContext("2d");
   //  6. make a new bar chart!
+  new Chart(ctx).Bar(myChartData);
 }
