7cc0ddd49948dbfe8b12ea6e2a0171ccc4a9adc9
reichardn <reichardn@gmail.com>
2016-08-06 00:42:38 -0400

    Done.

--- js/spotify-chart.js
+++ js/spotify-chart.js
@@ -17,28 +17,64 @@ $(function() {
 
 function extractTop10Tracks(tracks) {
   // your code here
+  var ans = []
+  for (var i = 0; i < 10; i++) {
+    ans.push(tracks[i])
+  }
+  return ans
 }
 
 function extractPopularity(tracks) {
   // your code here
+  var ans = []
+  for (i in tracks) {
+    ans.push(tracks[i].popularity)
+  }
+  return ans
 }
 
 function extractNames(tracks) {
   // your code here
+  var ans = []
+  for (i in tracks) {
+    ans.push(tracks[i].name)
+  }
+  return ans
 }
 
 function chartData(labels, inputData) {
   // your code here
-
+  var ans = {};
+  ans['labels'] = labels
+  dataSetProperties = {
+    fillColor: 'rgba(220,220,220,0.5)', 
+    strokeColor: 'rgba(220,220,220,0.8)', 
+    highlightFill: 'rgba(220,220,220,0.75)', 
+    highlightStroke: 'rgba(220,220,220,1)',
+    data: inputData
+  } 
+  ans['datasets'] = [dataSetProperties]
+  return ans
   // use the dataSetProperties variable defined above if it helps
 }
 
+function test(response) {
+  console.log('success!')
+}
+
+// Elvis id = 43ZHCT0cAZBISjO8DG9PnE
 function getSpotifyTracks(callback){
   // your ajax call here, on success it should call on the 
   // parameter it's passed (it's a function), and pass it's 
   // parameter the data it received
-
   // use the url variable defined above if it helps
+  var url = "https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/top-tracks?country=SE";
+  $.ajax({
+    url: url,
+    type: 'GET',
+    dataType: 'JSON',
+    success: callback
+  });
 }
 
 function success(parsedJSON) {
@@ -52,4 +88,13 @@ function success(parsedJSON) {
   //  5. make a variable `ctx` and select the canvas with the id of spotify-chart
   //     * also make sure to specify 2d context
   //  6. make a new bar chart!
+  var tracks = extractTop10Tracks(parsedJSON.tracks)
+  var names = extractNames(tracks);
+  var popularity = extractPopularity(tracks);
+  var data = chartData(names, popularity);
+  var ctx = $('#spotify-chart')
+  var myBarChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: data
+  });
 }
