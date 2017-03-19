c5038d6c919bacf68e713b17cef6eaae91170366
Joe <jsbenning4@gmail.com>
2017-03-19 09:08:41 -0400

    Done.

--- js/spotify-chart.js
+++ js/spotify-chart.js
@@ -7,69 +7,89 @@ var dataSetProperties = {
   highlightStroke: 'rgba(220,220,220,1)'
 };
 
-$(document).ready(getSpotifyTracks);
+//$(document).ready(getSpotifyTracks);
 //$(document).ready(testy);
 
-// $(function() {
-//     getSpotifyTracks();
-// });
+$(function() {
+    getSpotifyTracks(success);
+});
 
 // write functions to pass spec tests here outside the jQuery doc ready
 // then call function within doc ready to get them to work
 // and display the chart correctly in index.html
 
 function extractTop10Tracks(tracks) {
-  // your code here
-  if (tracks.length == 10) {
-    var top = tracks;
-  }
-  return top; 
+  //your code here
+  return tracks.slice(0, 10); 
 }
 
-function extractPopularity(tracks) {
+function extractPopularity(tracks) {  // better to use map method for these two functions
   // your code here
+  var myTracks = tracks 
   var myList = [];
-  tracks.forEach(function(element){
+  myTracks.forEach(function(element){
     myList.push(element.popularity);
   })
-  return(myList);
+  return myList;
 }
+
 
 function extractNames(tracks) {
   // your code here
+  var myTracks = tracks 
   var myList = [];
-  tracks.forEach(function(element){
+  myTracks.forEach(function(element){
     myList.push(element.name);
   })
-  return(myList);
+  return myList;
 }
+
 
 function chartData(labels, inputData) {
   //console.log(labels, inputData)
   //your code here
   //use the dataSetProperties variable defined above if it helps
-  var canvas = document.getElementById("spotify-chart").getContext("2d");
   var dsp = dataSetProperties;
+
+  var dataObj = {};
+  dataObj.labels = labels;
+  dataObj.datasets = [
+    {
+      fillColor:  dsp.fillColor,
+      strokeColor: dsp.strokeColor,
+      highlightFill: dsp.highlightFill,
+      highlightStroke: dsp.highlightStroke,
+      data: inputData
+    }
+  ];
+
+  return dataObj;
+
+} // "official" solution, which doesn't follow standard conventions in the chart.js documentation  
+
+//   var ctx = $("#spotify-chart").get(0).getContext("2d");
+//   var dsp = dataSetProperties;
   
-  var barData = {
-    labels: labels,
-    datasets: [
-      {
-        label: "Popularity",
-        fillColor:  dsp.fillColor,
-        strokeColor: dsp.strokeColor,
-        highlightFill: dsp.highlightFill,
-        highlightStroke: dsp.highlightStroke,
+//   var barData = {
+//     labels: labels,
+//     datasets: [
+//       {
+//         label: "Popularity",
+//         fillColor:  dsp.fillColor,
+//         strokeColor: dsp.strokeColor,
+//         highlightFill: dsp.highlightFill,
+//         highlightStroke: dsp.highlightStroke,
 
 
-        data: inputData
-      }
-    ]
-  }
-  new Chart(canvas).Bar(barData);
-} // end of alt version, which works like a charm, but doesn't pass tests
+//         data: inputData
+//       }
+//     ]
+//   }
+//   new Chart(ctx).Bar(barData);
 
-//original version, which follows specs and doumentation to the letter (or so I thought); fails tests, and doesn't render to boot
+// } // end of alt version, which works like a charm, but doesn't pass tests
+
+//original version, which follows specs and documentation to the letter (or so I thought); fails tests, and doesn't render to boot
 //   var canvas = document.getElementById("spotify-chart");
 //   var ctx = canvas.getContext("2d");
 //   var dsp = dataSetProperties;
@@ -97,31 +117,41 @@ function chartData(labels, inputData) {
 // }
 
 function getSpotifyTracks(callback){
-  // your ajax call here, on success it should call on the 
-  // parameter it's passed (it's a function), and pass its 
-  // parameter the data it received
+//   // your ajax call here, on success it should call on the 
+//   // parameter it's passed (it's a function), and pass its 
+//   // parameter the data it received
 
-  // use the url variable defined above if it helps
+//   //use the url variable defined above if it helps
+//   $.get(url).success(callback); // this is my alternate version, based on first part of Avi's video
 
+
+
+// // My original solution, which passes already parsed json ("tracks") to success
+//   // $.ajax({
+//   //   type: 'get',
+//   //   url: url,
+//   //   data: 'data',
+//   //   dataType: "JSON",
+
+//   //   }).done(function(data) {
+//   //   var alb = data.tracks;
+//   //   success(alb);
+//   //   })
+
+
+//   //}).fail(function(){
+//     //console.log("Something's broken!");
+//   //})
+// }
 
 
   $.ajax({
-    type: 'get',
     url: url,
-    data: 'data',
-    dataType: "JSON",
-
-    }).done(function(data) {
-    var alb = data.tracks;
-    //callback success(alb)
-    //alb.forEach(function(element){
-    //console.log(element.name + "-" + element.popularity);
-    success(alb);
-    })
-  //}).fail(function(){
-    //console.log("Something's broken!");
-  //})
-}
+    success: function(result) {
+      callback(result);
+    }
+  });
+} // the official solution
 
 function success(parsedJSON) {
   // this function will make a new bar chart, refer to this url:
@@ -134,22 +164,21 @@ function success(parsedJSON) {
   //  5. make a variable `ctx` and select the canvas with the id of spotify-chart
   //     * also make sure to specify 2d context
   //  6. make a new bar chart!
+  
 
-  var tracklist = extractTop10Tracks(parsedJSON);
-  var names = extractNames(tracklist);
-  var popularity = extractPopularity(tracklist);
+    var tracklist = extractTop10Tracks(parsedJSON.tracks);
+    var names = extractNames(tracklist);
+    var popularity = extractPopularity(tracklist);
 
-  chartData(names, popularity); 
+    var data = chartData(names, popularity); 
+    var ctx = document.getElementById("spotify-chart").getContext("2d");
+    new Chart(ctx).Bar(data);
 
 }
 
-// function testy() {
-//   var names = ["test1", "test2", "test3"];
-//   var popularity = [34, 44, 22];
 
-//   chartData(names, popularity); 
 
-// }
+
 
 
 
